9646b8eee8b7bcea8c15467853986c80c0bec68d
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2019-12-16 18:00:43 +0300

    feat: completed task

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -6,206 +6,185 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
- * Returns the functions composition of two specified functions f(x) and g(x).
- * The result of compose is to be a function of one argument, (lets call the argument x),
- * which works like applying function f to the result of applying function g to x, i.e.
- *  getComposition(f,g)(x) = f(g(x))
+ * Returns the rectagle object with width and height parameters and getArea() method
  *
- * @param {Function} f
- * @param {Function} g
- * @return {Function}
+ * @param {number} width
+ * @param {number} height
+ * @return {Object}
  *
  * @example
- *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
+ *    const r = new Rectangle(10,20);
+ *    console.log(r.width);       // => 10
+ *    console.log(r.height);      // => 20
+ *    console.log(r.getArea());   // => 200
+ */
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => width * height;
+}
+
+/**
+ * Returns the JSON representation of specified object
+ *
+ * @param {object} obj
+ * @return {string}
+ *
+ * @example
+ *    [1,2,3]   =>  '[1,2,3]'
+ *    { width: 10, height : 20 } => '{"height":10,"width":20}'
+ */
+function getJSON(obj) {
+  return JSON.stringify(obj);
+}
+
+/**
+ * Returns the object of specified type from JSON representation
+ *
+ * @param {Object} proto
+ * @param {string} json
+ * @return {object}
+ *
+ * @example
+ *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function getComposition(f, g) {
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
+}
+
+/**
+ * Css selectors builder
+ *
+ * Each complex selector can consists of type, id, class, attribute, pseudo-class
+ * and pseudo-element selectors:
+ *
+ *    element#id.class[attr]:pseudoClass::pseudoElement
+ *              \----/\----/\----------/
+ *              Can be several occurences
+ *
+ * All types of selectors can be combined using the combinators ' ','+','~','>' .
+ *
+ * The task is to design a single class, independent classes or classes hierarchy
+ * and implement the functionality to build the css selectors using the provided cssSelectorBuilder.
+ * Each selector should have the stringify() method to output the string repsentation
+ * according to css specification.
+ *
+ * Provided cssSelectorBuilder should be used as facade only to create your own classes,
+ * for example the first method of cssSelectorBuilder can be like this:
+ *   element: function(value) {
+ *       return new MySuperBaseElementSelector(...)...
+ *   },
+ *
+ * The design of class(es) is totally up to you, but try to make it as simple,
+ * clear and readable as possible.
+ *
+ * @example
+ *
+ *  const builder = cssSelectorBuilder;
+ *
+ *  builder.id('main').class('container').class('editable').stringify()
+ *    => '#main.container.editable'
+ *
+ *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()
+ *    => 'a[href$=".png"]:focus'
+ *
+ *  builder.combine(
+ *      builder.element('div').id('main').class('container').class('draggable'),
+ *      '+',
+ *      builder.combine(
+ *          builder.element('table').id('data'),
+ *          '~',
+ *           builder.combine(
+ *               builder.element('tr').pseudoClass('nth-of-type(even)'),
+ *               ' ',
+ *               builder.element('td').pseudoClass('nth-of-type(even)')
+ *           )
+ *      )
+ *  ).stringify()
+ *    => 'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
+ *
+ *  For more examples see unit tests.
+ * https://www.codewars.com/kata/css-selectors-builder/train/javascript
+ */
+class CssSelector {
+  constructor() {
+    this.selector = '';
+    this.type = 0;
+    this.uniqueError = 'Element, id and pseudo-element should not occur more then one time inside the selector'; // eslint-disable-line
+    this.orderError = // eslint-disable-line
+      'Selector parts should be arranged in the following order: ' + // eslint-disable-line
+      'element, id, class, attribute, pseudo-class, pseudo-element'; // eslint-disable-line
+  }
+
+  element(value) {
+    this.checkType(1);
+    this.selector += value;
+    return this;
+  }
+
+  id(value) {
+    this.checkType(2);
+    this.selector += `#${value}`;
+    return this;
+  }
+
+  class(value) {
+    this.checkType(3);
+    this.selector += `.${value}`;
+    return this;
+  }
+
+  attr(value) {
+    this.checkType(4);
+    this.selector += `[${value}]`;
+    return this;
+  }
+
+  pseudoClass(value) {
+    this.checkType(5);
+    this.selector += `:${value}`;
+    return this;
+  }
+
+  pseudoElement(value) {
+    this.checkType(6);
+    this.selector += `::${value}`;
+    return this;
+  }
+
+  combine(selector1, combinator, selector2) {
+    this.selector = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return this;
+  }
+
+  stringify() {
+    return this.selector;
+  }
+
+  checkType(type) {
+    if (type < this.type) throw this.orderError;
+    if (type === this.type && [1, 2, 6].includes(type)) throw this.uniqueError;
+    this.type = type;
+  }
+}
+
+const cssSelectorBuilder = {
   // prettier-ignore
-  return (x) => f(g.call(this, x));
-}
-
-/**
- * Returns the math power function with the specified exponent
- *
- * @param {number} exponent
- * @return {Function}
- *
- * @example
- *   const power2 = getPowerFunction(2); // => x^2
- *   power2(2) => 4
- *   power2(4) => 16
- *
- *   const power05 = getPowerFunction(0.5); // => x^0.5
- *   power05(4) => 2
- *   power05(16) => 4
- *
- */
-function getPowerFunction(exponent) {
+  element: (value) => new CssSelector().element(value),
   // prettier-ignore
-  return (x) => x ** exponent;
-}
-
-/**
- * Returns the polynom function of one argument based on specified coefficients.
- * See: https://en.wikipedia.org/wiki/Polynomial#Definition
- *
- * @params {integer}
- * @return {Function}
- *
- * @example
- *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
- *   getPolynom(1,-3)  => y = x - 3
- *   getPolynom(8)     => y = 8
- *   getPolynom()      => null
- */
-function getPolynom(...args) {
+  id: (value) => new CssSelector().id(value),
   // prettier-ignore
-  return (x) => {
-    let res = 0;
-    for (let i = args.length - 1; i >= 0; i -= 1) {
-      res += args[i] * (x ** (args.length - i - 1));
-    }
-    return res;
-  };
-}
-
-/**
- * Memoizes passed function and returns function
- * which invoked first time calls the passed function and then always returns cached result.
- *
- * @params {Function} func - function to memoize
- * @return {Function} memoized function
- *
- * @example
- *   const memoizer = memoize(() => Math.random());
- *   memoizer() => some random number  (first run, evaluates the result of Math.random())
- *   memoizer() => the same random number  (second run, returns the previous cached result)
- *   ...
- *   memoizer() => the same random number  (next run, returns the previous cached result)
- */
-function memoize(func) {
-  const cache = new Map();
+  class: (value) => new CssSelector().class(value),
   // prettier-ignore
-  return (x) => {
-    if (cache.has(x)) return cache.get(x);
-    const result = func.call(this, x);
-    cache.set(x, result);
-    return result;
-  };
-}
-
-/**
- * Returns the function trying to call the passed function and if it throws,
- * retrying it specified number of attempts.
- *
- * @param {Function} func
- * @param {number} attempts
- * @return {Function}
- *
- * @example
- * const attempt = 0, retryer = retry(() => {
- *      if (++attempt % 2) throw new Error('test');
- *      else return attempt;
- * }, 2);
- * retryer() => 2
- */
-function retry(func, attempts) {
-  return () => {
-    let attemptThere = 0;
-    while (attemptThere < attempts) {
-      try {
-        return func();
-      } catch (e) {
-        attemptThere += 1;
-      }
-    }
-    return new Error();
-  };
-}
-
-/**
- * Returns the logging wrapper for the specified method,
- * Logger has to log the start and end of calling the specified function.
- * Logger has to log the arguments of invoked function.
- * The fromat of output log is:
- * <function name>(<arg1>, <arg2>,...,<argN>) starts
- * <function name>(<arg1>, <arg2>,...,<argN>) ends
- *
- *
- * @param {Function} func
- * @param {Function} logFunc - function to output log with single string argument
- * @return {Function}
- *
- * @example
- *
- * const cosLogger = logger(Math.cos, console.log);
- * const result = cosLogger(Math.PI));     // -1
- *
- * log from console.log:
- * cos(3.141592653589793) starts
- * cos(3.141592653589793) ends
- *
- */
-function logger(func, logFunc) {
-  return (...args) => {
-    // prettier-ignore
-    const str = args.map((el) => JSON.stringify(el)).join(',');
-
-    logFunc(`${func.name}(${str}) starts`);
-    const res = func(...args);
-    logFunc(`${func.name}(${str}) ends`);
-
-    return res;
-  };
-}
-
-/**
- * Return the function with partial applied arguments
- *
- * @param {Function} fn
- * @return {Function}
- *
- * @example
- *   const fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
- *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
- */
-function partialUsingArguments(fn, ...args1) {
-  return (...args) => {
-    const resArgs = args1.concat(args);
-    return fn(...resArgs);
-  };
-}
-
-/**
- * Returns the id generator function that returns next integer starting
- * from specified number every time when invoking.
- *
- * @param {Number} startFrom
- * @return {Function}
- *
- * @example
- *   const getId4 = getIdGenerator(4);
- *   const getId10 = gerIdGenerator(10);
- *   getId4() => 4
- *   getId10() => 10
- *   getId4() => 5
- *   getId4() => 6
- *   getId4() => 7
- *   getId10() => 11
- */
-function getIdGeneratorFunction(startFrom) {
-  let count = startFrom - 1; // eslint-disable-line
-  return () => {
-    count += 1;
-    return count;
-  };
-}
-
+  attr: (value) => new CssSelector().attr(value),
+  // prettier-ignore
+  pseudoClass: (value) => new CssSelector().pseudoClass(value),
+  // prettier-ignore
+  pseudoElement: (value) => new CssSelector().pseudoElement(value),
+  combine: (s1, c, s2) => new CssSelector().combine(s1, c, s2),
+};
 
 module.exports = {
   Rectangle,
--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -5,8 +5,37 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
-});
+/**
+ * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
+ * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
+ * if boolean value === false is passed, and rejected
+ * with error message === 'Wrong parameter is passed! Ask her again.',
+ * if is not boolean value passed
+ *
+ * @param {boolean} isPositiveAnswer
+ * @return {Promise}
+ *
+ * @example
+ *    const p1 = willYouMarryMe(true);
+ *    p1.then(answer => console.log(answer)) // 'Hooray!!! She said "Yes"!'
+ *
+ *    const p2 = willYouMarryMe(false);
+ *    p2.then(answer => console.log(answer)) // 'Oh no, she said "No".';
+ *
+ *    const p3 = willYouMarryMe();
+ *    p3.then(answer => console.log(answer))
+ *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
+ *                                                    //  Ask her again.';
+ */
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer === 'boolean') {
+      const ans = isPositiveAnswer ? 'Hooray!!! She said "Yes"!' : 'Oh no, she said "No".';
+      resolve(ans);
+    } else {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  });
 }
 
 /**
